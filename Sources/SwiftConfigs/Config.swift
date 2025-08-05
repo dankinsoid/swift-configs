7ed080808032bec9fd93c7f806ee5f6181939f70
Danil <voidilov@gmail.com>
2025-08-05 22:14:35 +0300

    inits

--- Sources/SwiftConfigs/Config.swift
+++ Sources/SwiftConfigs/Config.swift
@@ -1,45 +1,57 @@
 import Foundation
 
-@propertyWrapper
-public struct Config<Key: ConfigKey> {
-	public let configs: Configs
-	public let key: KeyPath<Configs.Keys, Key>
+public protocol ConfigWrapper<Key> {
+	
+	associatedtype Key: ConfigKey
+	var configs: Configs { get }
+	var key: Key { get }
+	init(_ key: Key, configs: Configs)
+}
 
-	public var wrappedValue: Key.Value {
-		configs[dynamicMember: key]
+public extension ConfigWrapper {
+	
+	init(_ key: KeyPath<Configs.Keys, Key>, configs: Configs = Configs()) {
+		self.init(Configs.Keys()[keyPath: key], configs: Configs())
 	}
 	
-	public var projectedValue: Config {
+	var projectedValue: Self {
 		self
 	}
-
-	public init(_ key: KeyPath<Configs.Keys, Key>, configs: Configs = Configs()) {
-		self.key = key
-		self.configs = configs
-	}
 	
-	public func exists() -> Bool {
+	func exists() -> Bool {
 		configs.exists(key)
 	}
 }
 
 @propertyWrapper
-public struct WritableConfig<Key: WritableConfigKey> {
+public struct Config<Key: ConfigKey>: ConfigWrapper {
+
 	public let configs: Configs
-	public let key: KeyPath<Configs.Keys, Key>
+	public let key: Key
 
 	public var wrappedValue: Key.Value {
-		get { configs[dynamicMember: key] }
-		nonmutating set {
-			configs[dynamicMember: key] = newValue
-		}
+		configs.get(key)
 	}
 
-	public var projectedValue: WritableConfig {
-		self
+	public init(_ key: Key, configs: Configs) {
+		self.key = key
+		self.configs = configs
+	}
+}
+
+@propertyWrapper
+public struct WritableConfig<Key: WritableConfigKey>: ConfigWrapper {
+	public let configs: Configs
+	public let key: Key
+
+	public var wrappedValue: Key.Value {
+		get { configs.get(key) }
+		nonmutating set {
+			configs.set(key, newValue)
+		}
 	}
 	
-	public init(_ key: KeyPath<Configs.Keys, Key>, configs: Configs = Configs()) {
+	public init(_ key: Key, configs: Configs) {
 		self.key = key
 		self.configs = configs
 	}
@@ -47,8 +59,76 @@ public struct WritableConfig<Key: WritableConfigKey> {
 	public func remove() throws {
 		try configs.remove(key)
 	}
-	
-	public func exists() -> Bool {
-		configs.exists(key)
+}
+
+public extension ConfigWrapper where Key.Value: LosslessStringConvertible {
+
+	init(
+		_ key: String,
+		in category: ConfigsCategory,
+		cacheDefaultValue: Bool = false,
+		wrappedValue defaultValue: @escaping @autoclosure () -> Key.Value
+	) {
+		self.init(
+			Key(
+				key,
+				handler: { $0.handler(for: category) },
+				as: .stringConvertable,
+				default: defaultValue(),
+				cacheDefaultValue: false
+			),
+			configs: Configs()
+		)
+	}
+}
+
+public extension ConfigWrapper where Key.Value: RawRepresentable, Key.Value.RawValue: LosslessStringConvertible {
+
+	init(
+		_ key: String,
+		in category: ConfigsCategory,
+		cacheDefaultValue: Bool = false,
+		wrappedValue defaultValue: @escaping @autoclosure () -> Key.Value
+	) {
+		self.init(
+			Key(
+				key,
+				in: category,
+				as: .rawRepresentable,
+				default: defaultValue(),
+				cacheDefaultValue: false
+			),
+			configs: Configs()
+		)
+	}
+}
+
+public extension ConfigWrapper where Key.Value: Codable {
+
+	/// Returns the key instance.
+	///
+	/// - Parameters:
+	///   - key: The key string.
+	///   - default: The default value to use if the key is not found.
+	///   - decoder: The JSON decoder to use for decoding the value.
+	@_disfavoredOverload
+	init(
+		_ key: String,
+		in category: ConfigsCategory,
+		cacheDefaultValue: Bool = false,
+		decoder: JSONDecoder = JSONDecoder(),
+		encoder: JSONEncoder = JSONEncoder(),
+		wrappedValue defaultValue: @escaping @autoclosure () -> Key.Value
+	) {
+		self.init(
+			Key(
+				key,
+				in: category,
+				as: .json(decoder: decoder, encoder: encoder),
+				default: defaultValue(),
+				cacheDefaultValue: cacheDefaultValue
+			),
+			configs: Configs()
+		)
 	}
 }
